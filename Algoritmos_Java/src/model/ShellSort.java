4e95b0fe343cc4023a2ed5734cf33695be3a4c44
LvMono <lvzack04@gmail.com>
2026-08-22 18:32:28 -0500

    Update ShellSort.java
    Implemented the ShellSort algorithm to sort an array using a gap-based insertion sort approach.

--- Algoritmos_Java/src/model/ShellSort.java
+++ Algoritmos_Java/src/model/ShellSort.java
@@ -2,4 +2,33 @@ package model;
 
 public class ShellSort {
 
+    public void ordenar(int[] arr) {
+        int n = arr.length;
+
+        // inicia la brecha en la mitad del tamano y se reduce a la mitad en cada ciclo
+        
+        for (int brecha = n / 2; brecha > 0; brecha /= 2) {
+            
+            // hace un ordenamiento por insercion para la brecha actual
+        	
+            for (int i = brecha; i < n; i++) {
+                
+                // guarda el valor actual a comparar
+            	
+                int temp = arr[i];
+                
+                int j;
+                
+                // desplaza los elementos anteriores que sean mayores que temp
+                
+                for (j = i; j >= brecha && arr[j - brecha] > temp; j -= brecha) {
+                    arr[j] = arr[j - brecha];
+                }
+                
+                // coloca el valor temporal en su posicion correcta
+                
+                arr[j] = temp;
+            }
+        }
+    }
 }
