1c04bc3c7787a2c11c3ab98b635b018338ce68ac
LvMono <lvzack04@gmail.com>
2026-08-23 15:55:01 -0500

    Update Main.java

--- Algoritmos_Java/src/controller/Main.java
+++ Algoritmos_Java/src/controller/Main.java
@@ -1,5 +1,12 @@
-package controller;
+package controller; 
 
 public class Main {
 
+    public static void main(String[] args) {
+
+    	// crea el controlador e inicia
+        
+    	Controlador controlador = new Controlador();
+        controlador.iniciar();
+    }
 }
